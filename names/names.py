3ea33969afeccad3b43186e061651470682d6185
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-06-14 11:05:35 -0400

    optimized

--- names/names.py
+++ names/names.py
@@ -11,10 +11,17 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+dups = {}
 for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    dups[name_1] = True
+
+for name_2 in  names_2:
+    if name_2 in dups:
+        duplicates.append(name_2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
